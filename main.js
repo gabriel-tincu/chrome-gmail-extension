98196ea000c25ef21576167684ffc84523d49d12
Tincu Gabriel <tincu@eloquentix.com>
2016-05-20 14:51:49 +0300

    Properly setting class on classifying links; Implementing the fail/retry logic

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var gmail;
-
+var retries_count = 3;
 
 function refresh(f) {
   if( (/in/.test(document.readyState)) || (typeof Gmail === undefined) ) {
@@ -13,11 +13,10 @@ var get_email_data = function() {
   // this function should be called only after checking that we're inside an actual email
   var gmail = new Gmail();
     if(gmail.check.is_inside_email()){
-    var user_mail = gmail.get.user_email();
-    var mail_body = gmail.get.email_source();
     var response = {};
-    response["mail_body"] = mail_body;
-    response["mail_address"] = user_mail;
+    response["mail_body"] = gmail.get.email_source();;
+    response["mail_address"] = gmail.get.user_email();
+    response["email_id"] = gmail.get.email_id();
     return response;
   } else {
     return null;
@@ -28,12 +27,19 @@ var post_email = function(is_receipt) {
   var r = get_email_data();
   if(r != null && r != "null"){
     r['is_receipt'] = is_receipt;
-    var url = "http://localhost:9999/foo"
-    console.log(JSON.stringify(r));
-    // i'll have to write a function to deal with failure / success (alert the user maybe???)
-    $.post(url, r);
+    var url = "https://localhost:9999/foo"
+    post_with_retries(url, r, retries_count);
   } else {
     alert("Not inside an email !")
+  }
+}
+
+var post_with_retries = function(url, data, retries){
+  console.log(retries + " retries left");
+  if(retries==0){
+    alert("Giving up on email with id "+data["email_id"]+" after exhausting "+retries_count+" retries");
+  } else {
+    $.post(url, data).fail(function(){ post_with_retries(url, data, retries-1); });
   }
 }
 
@@ -47,13 +53,17 @@ var post_non_receipt = function() {
 
 var main = function(){
   var send_receipt = document.createElement("a");
-  send_receipt.set_attribute("class", "custom_anchor");
-  send_non_receipt.set_attribute("class", "custom_anchor");
   var send_non_receipt = document.createElement("a");
+  
+  send_receipt.className = "receipt_link"
+  send_non_receipt.className = "receipt_link"
+
   send_receipt.appendChild(document.createTextNode("RECEIPT   "));
   send_non_receipt.appendChild(document.createTextNode("NON RECEIPT"));
-  send_receipt.addEventListener("click", post_receipt);
-  send_non_receipt.addEventListener("click", post_non_receipt);
+  
+  send_receipt.href = "javascript:post_receipt()";
+  send_non_receipt.href = "javascript:post_non_receipt()";
+  
   document.body.insertBefore(send_non_receipt, document.body.childNodes[0]);
   document.body.insertBefore(send_receipt, document.body.childNodes[0]);
 }
